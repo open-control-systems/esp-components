7c734385651dc2bcaaa9e5c34b77b2d1a8a70ab6
Dmitriy Shilin <dshil@fastmail.com>
2024-07-05 13:59:15 +0300

    Rework project structure

---
+++ main/main.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+
+#include "esp_log.h"
+
+#include "ocs_net/mdns_provider.h"
+#include "ocs_net/wifi_network.h"
+#include "ocs_status/code.h"
+#include "ocs_status/code_to_str.h"
+#include "ocs_storage/flash_storage.h"
+
+namespace {
+
+const char* log_tag = "app-main";
+
+} // namespace
+
+using namespace ocs;
+
+extern "C" void app_main(void) {
+    storage::FlashStorage storage;
+
+    net::WiFiNetwork wifi_network(net::WiFiNetwork::Params {
+        .max_retry_count = 32,
+        .ssid = "foo",
+        .password = "password",
+    });
+
+    net::MDNSProvider mdns_provider(net::MDNSProvider::Params {
+        .hostname = "foo",
+        .instance_name = "bar",
+    });
+    const auto status = mdns_provider.add_service("foo", "bar", 80);
+    ESP_LOGI(log_tag, "status=%s", status::code_to_str(status));
+
+    while (true) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
