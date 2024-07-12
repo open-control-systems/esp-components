8aa35a342138bb099931bb36baebf304b33d7263
Dmitriy Shilin <dshil@fastmail.com>
2024-07-12 14:58:01 +0300

    Allow to get received IP address

---
+++ components/ocs_net/ip_addr_to_str.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <cstring>
+
+#include "lwip/inet.h"
+
+#include "ocs_net/ip_addr_to_str.h"
+
+namespace ocs {
+namespace net {
+
+ip_addr_to_str::ip_addr_to_str(ip_addr_t addr) {
+    if (!inet_ntoa_r(addr, buf_, sizeof(buf_))) {
+        strcpy(buf_, "undefined");
+    }
+}
+
+const char* ip_addr_to_str::c_str() const {
+    return buf_;
+}
+
+} // namespace net
+} // namespace ocs
