b93c2b9e12c904c36a5e98bdf1176bb00ce1e9cc
Dmitriy Shilin <dshil@fastmail.com>
2024-07-16 16:15:35 +0300

    Simplify data serving over HTTP via GET method

---
+++ components/ocs_iot/default_http_handler.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include "esp_log.h"
+
+#include "ocs_core/noncopyable.h"
+#include "ocs_iot/cjson_builder.h"
+#include "ocs_iot/default_json_formatter.h"
+#include "ocs_net/http_server.h"
+
+namespace ocs {
+namespace iot {
+
+template <unsigned Size>
+class DefaultHTTPHandler : public core::NonCopyable<DefaultHTTPHandler<Size>> {
+public:
+    //! Initialize.
+    //!
+    //! @params
+    //!  - @p server to register the HTTP endpoint.
+    //!  - @p formatter to format the data.
+    //!  - @p path - URI path.
+    //!  - @p log_tag - ESP-IDF log tag.
+    DefaultHTTPHandler(net::HTTPServer& server,
+                       IJSONFormatter& formatter,
+                       const char* path,
+                       const char* log_tag) {
+        json_formatter_.reset(new (std::nothrow) JSONFormatter(formatter));
+
+        server.add_GET(path, [this, path, log_tag](httpd_req_t* req) {
+            auto json = cJSONUniqueBuilder::make_json();
+            json_formatter_->format(json.get());
+
+            const auto err =
+                httpd_resp_send(req, json_formatter_->c_str(), HTTPD_RESP_USE_STRLEN);
+            if (err != ESP_OK) {
+                ESP_LOGE(log_tag, "httpd_resp_send(): %s", esp_err_to_name(err));
+                return status::StatusCode::Error;
+            }
+
+            return status::StatusCode::OK;
+        });
+    }
+
+private:
+    using JSONFormatter = DefaultJSONFormatter<Size>;
+
+    std::unique_ptr<JSONFormatter> json_formatter_;
+};
+
+} // namespace iot
+} // namespace ocs
