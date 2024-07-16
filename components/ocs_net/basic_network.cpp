2731ab34fe0bd654c24764e4ba5e4817b38b7d9f
Dmitriy Shilin <dshil@fastmail.com>
2024-07-16 14:39:56 +0300

    Add missed LICENSE

--- components/ocs_net/basic_network.cpp
+++ components/ocs_net/basic_network.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #include "ocs_net/basic_network.h"
 
 namespace ocs {
