900a2e5470a8086b61a2b953aa7c06838820b87d
Dmitriy Shilin <dshil@fastmail.com>
2024-08-07 09:05:51 +0200

    Introduce component to handle telemetry and registration data

---
+++ components/ocs_iot/json_data_pipeline.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "ocs_iot/json_data_pipeline.h"
+
+namespace ocs {
+namespace iot {
+
+JsonDataPipeline::JsonDataPipeline(core::IClock& clock,
+                                   storage::StorageBuilder& storage_builder,
+                                   scheduler::AsyncTaskScheduler& task_scheduler,
+                                   scheduler::TimerStore& timer_store,
+                                   system::FanoutRebootHandler& reboot_handler) {
+    telemetry_formatter_.reset(new (std::nothrow) TelemetryJsonFormatter());
+    configASSERT(telemetry_formatter_);
+
+    registration_formatter_.reset(new (std::nothrow) RegistrationJsonFormatter());
+    configASSERT(registration_formatter_);
+
+    system_counter_storage_ = storage_builder.make("system_counter");
+    configASSERT(system_counter_storage_);
+
+    counter_json_formatter_.reset(new (std::nothrow) CounterJsonFormatter());
+    configASSERT(counter_json_formatter_);
+
+    system_counter_pipeline_.reset(new (std::nothrow) SystemCounterPipeline(
+        clock, *system_counter_storage_, reboot_handler, task_scheduler, timer_store,
+        *counter_json_formatter_));
+    configASSERT(system_counter_pipeline_);
+
+    telemetry_formatter_->get_fanout_formatter().add(*counter_json_formatter_);
+}
+
+FanoutJsonFormatter& JsonDataPipeline::get_telemetry_formatter() {
+    return telemetry_formatter_->get_fanout_formatter();
+}
+
+FanoutJsonFormatter& JsonDataPipeline::get_registration_formatter() {
+    return registration_formatter_->get_fanout_formatter();
+}
+
+diagnostic::BasicCounterHolder& JsonDataPipeline::get_counter_holder() {
+    return *counter_json_formatter_;
+}
+
+} // namespace iot
+} // namespace ocs
---
+++ components/ocs_iot/telemetry_json_formatter.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "freertos/FreeRTOSConfig.h"
+
+#include "ocs_iot/system_json_formatter.h"
+#include "ocs_iot/telemetry_json_formatter.h"
+
+namespace ocs {
+namespace iot {
+
+TelemetryJsonFormatter::TelemetryJsonFormatter() {
+    fanout_formatter_.reset(new (std::nothrow) FanoutJsonFormatter());
+    configASSERT(fanout_formatter_);
+
+    system_formatter_.reset(new (std::nothrow) SystemJsonFormatter());
+    configASSERT(system_formatter_);
+
+    fanout_formatter_->add(*system_formatter_);
+}
+
+void TelemetryJsonFormatter::format(cJSON* json) {
+    fanout_formatter_->format(json);
+}
+
+FanoutJsonFormatter& TelemetryJsonFormatter::get_fanout_formatter() {
+    return *fanout_formatter_;
+}
+
+} // namespace iot
+} // namespace ocs
