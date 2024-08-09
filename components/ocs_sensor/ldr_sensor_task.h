a7160c60b9469120ac59258e4291a72a1c664900
Dmitriy Shilin <dshil@fastmail.com>
2024-08-09 16:55:48 +0200

    Introduce LDR sensor

---
+++ components/ocs_sensor/ldr_sensor_task.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include "ocs_core/noncopyable.h"
+#include "ocs_io/adc_store.h"
+#include "ocs_scheduler/async_task_scheduler.h"
+#include "ocs_scheduler/timer_store.h"
+#include "ocs_sensor/basic_sensor_task.h"
+#include "ocs_sensor/ldr_sensor.h"
+
+namespace ocs {
+namespace sensor {
+
+class LdrSensorTask : public BasicSensorTask<LdrSensor>, public core::NonCopyable<> {
+public:
+    //! Initialize.
+    LdrSensorTask(io::AdcStore& adc_store,
+                  scheduler::AsyncTaskScheduler& task_scheduler,
+                  scheduler::TimerStore& timer_store);
+};
+
+} // namespace sensor
+} // namespace ocs
