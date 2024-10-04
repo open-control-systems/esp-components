5a66eea6d40e368a1926080a9bcdba840a82d8d8
Dmitriy Shilin <dshil@fastmail.com>
2024-10-04 08:47:32 +0200

    Simplify LDR pipeline

--- components/ocs_sensor/ldr/sensor_pipeline.h
+++ components/ocs_sensor/ldr/sensor_pipeline.h
@@ -9,6 +9,7 @@
 #pragma once
 
 #include <memory>
+#include <string>
 
 #include "ocs_core/noncopyable.h"
 #include "ocs_core/time.h"
@@ -28,16 +29,23 @@ public:
     };
 
     //! Initialize.
+    //!
+    //! @params
+    //!  - @p adc_store to perform ADC readings.
+    //!  - @p task_scheduler to schedule periodic ADC readings.
+    //!  - @p id to distringuish one sensor from another.
     SensorPipeline(io::AdcStore& adc_store,
                    scheduler::ITaskScheduler& task_scheduler,
-                   const char* sensor_id,
-                   const char* task_id,
+                   const char* id,
                    Params params);
 
     //! Return the underlying sensor.
     Sensor& get_sensor();
 
 private:
+    const std::string sensor_id_;
+    const std::string task_id_;
+
     std::unique_ptr<Sensor> sensor_;
 };
 
