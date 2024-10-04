4d6966696e9cf42acda5b30d6f8e367adf8bb250
Dmitriy Shilin <dshil@fastmail.com>
2024-10-04 11:04:49 +0200

    Revert "Use lambda formatter for DS18B20 JSON formatting"
    This reverts commit b27200a61e6352867d2e0c122f6380cb659f2d33.

---
+++ components/ocs_pipeline/ds18b20/json_formatter.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "ocs_pipeline/ds18b20/json_formatter.h"
+#include "ocs_fmt/json/cjson_object_formatter.h"
+
+namespace ocs {
+namespace pipeline {
+namespace ds18b20 {
+
+JsonFormatter::JsonFormatter(sensor::ds18b20::Sensor& sensor)
+    : sensor_(sensor) {
+}
+
+status::StatusCode JsonFormatter::format(cJSON* json) {
+    fmt::json::CjsonObjectFormatter formatter(json);
+
+    if (!formatter.add_number_cs(sensor_.id(), sensor_.get_data())) {
+        return status::StatusCode::NoMem;
+    }
+
+    return status::StatusCode::OK;
+}
+
+} // namespace ds18b20
+} // namespace pipeline
+} // namespace ocs
---
+++ components/ocs_pipeline/ds18b20/json_formatter.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <string>
+
+#include "ocs_core/noncopyable.h"
+#include "ocs_fmt/json/iformatter.h"
+#include "ocs_sensor/ds18b20/sensor.h"
+
+namespace ocs {
+namespace pipeline {
+namespace ds18b20 {
+
+class JsonFormatter : public fmt::json::IFormatter, public core::NonCopyable<> {
+public:
+    //! Initialize.
+    //!
+    //! @params
+    //!  - @p sensor to read temperature.
+    explicit JsonFormatter(sensor::ds18b20::Sensor& sensor);
+
+    //! Format DS18B20 sensor data into @p json.
+    status::StatusCode format(cJSON* json) override;
+
+private:
+    sensor::ds18b20::Sensor& sensor_;
+};
+
+} // namespace ds18b20
+} // namespace pipeline
+} // namespace ocs
