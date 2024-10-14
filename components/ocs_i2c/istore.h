f2fbf2a0370c3384193452371203b634c36ed048
Dmitriy Shilin <dshil@fastmail.com>
2024-10-14 15:02:46 +0200

    Introduce I2C sub-system

---
+++ components/ocs_i2c/istore.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include "ocs_i2c/itransceiver.h"
+
+namespace ocs {
+namespace i2c {
+
+class IStore {
+public:
+    enum class TransferSpeed : uint8_t {
+        //! 100 kbit/s.
+        Default,
+
+        //! 400 kbit/s.
+        Fast,
+    };
+
+    enum class AddressLength : uint8_t {
+        //! 7-bit I2C address.
+        Bit_7,
+
+        //! 10-bit I2C address.
+        Bit_10,
+    };
+
+    //! I2C address.
+    using Address = uint16_t;
+
+    //! I2C transceiver to communicate with I2C device.
+    using ITransceiverPtr = std::unique_ptr<ITransceiver>;
+
+    //! Destroy.
+    virtual ~IStore() = default;
+
+    //! Register a new I2C device.
+    //!
+    //! @params
+    //!  - @p id - to distringuish one I2C device from another.
+    //!  - @p len - I2C device address length.
+    //!  - @p addr - actual I2C device address.
+    //!  - @p speed - how fast data will be transmitted over I2C bus.
+    virtual ITransceiverPtr
+    add(const char* id, AddressLength len, Address addr, TransferSpeed speed) = 0;
+};
+
+} // namespace i2c
+} // namespace ocs
---
+++ components/ocs_i2c/master_transceiver.cpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "ocs_i2c/master_transceiver.h"
+#include "ocs_core/log.h"
+
+namespace ocs {
+namespace i2c {
+
+namespace {
+
+const char* log_tag = "master_transceiver";
+
+} // namespace
+
+MasterTransceiver::DevicePtr
+MasterTransceiver::make_device_shared(i2c_master_dev_t* device) {
+    return MasterTransceiver::DevicePtr(device, [](i2c_master_dev_t* device) {
+        if (device) {
+            const auto err = i2c_master_bus_rm_device(device);
+            if (err != ESP_OK) {
+                ocs_loge(log_tag, "i2c_master_bus_rm_device() %s", esp_err_to_name(err));
+            }
+        }
+    });
+}
+
+MasterTransceiver::MasterTransceiver(MasterTransceiver::DevicePtr device, const char* id)
+    : id_(id)
+    , device_(device) {
+}
+
+status::StatusCode
+MasterTransceiver::send(const uint8_t* buf, unsigned size, core::microseconds_t timeout) {
+    if (timeout > 0 && timeout < core::Millisecond) {
+        return status::StatusCode::InvalidArg;
+    }
+
+    const auto err = i2c_master_transmit(device_.get(), buf, size, timeout);
+    if (err != ESP_OK) {
+        ocs_logi(log_tag, "i2c_master_transmit() failed: id=%s err=%s", id_.c_str(),
+                 esp_err_to_name(err));
+
+        if (err == ESP_ERR_INVALID_ARG) {
+            return status::StatusCode::InvalidArg;
+        }
+        if (err == ESP_ERR_TIMEOUT) {
+            return status::StatusCode::Timeout;
+        }
+
+        return status::StatusCode::Error;
+    }
+
+    return status::StatusCode::OK;
+}
+
+status::StatusCode
+MasterTransceiver::receive(uint8_t* buf, unsigned size, core::microseconds_t timeout) {
+    if (timeout > 0 && timeout < core::Millisecond) {
+        return status::StatusCode::InvalidArg;
+    }
+
+    const auto err = i2c_master_receive(device_.get(), buf, size, timeout);
+    if (err != ESP_OK) {
+        ocs_logi(log_tag, "i2c_master_receive() failed: id=%s err=%s", id_.c_str(),
+                 esp_err_to_name(err));
+
+        if (err == ESP_ERR_INVALID_ARG) {
+            return status::StatusCode::InvalidArg;
+        }
+        if (err == ESP_ERR_TIMEOUT) {
+            return status::StatusCode::Timeout;
+        }
+
+        return status::StatusCode::Error;
+    }
+
+    return status::StatusCode::OK;
+}
+
+} // namespace i2c
+} // namespace ocs
