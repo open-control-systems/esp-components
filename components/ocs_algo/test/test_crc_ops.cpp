aed5ae1533ce7096b6da2a833af6e46df941b3b9
Dmitriy Shilin <dshil@fastmail.com>
2024-10-14 14:48:39 +0200

    Generalize CRC-8 checksum calculation

---
+++ components/ocs_algo/test/test_crc_ops.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <cstring>
+
+#include "unity.h"
+
+#include "ocs_algo/crc_ops.h"
+#include "ocs_core/bit_ops.h"
+
+namespace ocs {
+namespace algo {
+
+TEST_CASE("CRC Ops: crc8: MSB", "[ocs_algo], [crc_ops]") {
+    const uint16_t data = 0xBEEF;
+
+    uint8_t buf[2];
+    memset(buf, 0, sizeof(buf));
+
+    core::BitOps::unpack_uint16(data, buf[0], buf[1]);
+
+    TEST_ASSERT_EQUAL(0x92,
+                      CrcOps::crc8(buf, sizeof(buf), 0xFF, 0x31, CrcOps::BitOrder::MSB));
+}
+
+TEST_CASE("CRC Ops: crc8: LSB", "[ocs_algo], [crc_ops]") {
+    const uint8_t buf[] {
+        0xAB, 0x1, 0x4B, 0x46, 0x7F, 0xFF, 0x5, 0x10,
+    };
+
+    TEST_ASSERT_EQUAL(0x92,
+                      CrcOps::crc8(buf, sizeof(buf), 0x00, 0x8C, CrcOps::BitOrder::LSB));
+}
+
+} // namespace algo
+} // namespace ocs
