25b0fb9dbee42621e08d4ed8c47b736cf81938bf
Dmitriy Shilin <dshil@fastmail.com>
2024-10-30 12:43:35 +0100

    Simplify basic checks in CI

---
+++ tools/scripts/check_license.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -xe
+
+output=$("$OCS_COMPONENTS_PATH"/tools/scripts/verify_license.py --path "$PROJECT_PATH" 2>&1 || exit 1)
+if [ -n "$output" ]; then
+  echo "Error: Some files are missing the MPL license header:"
+  echo "$output"
+  exit 1
+fi
