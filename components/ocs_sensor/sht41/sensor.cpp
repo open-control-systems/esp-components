1ddfe110f1999388e73a830b21519ed3a98d91af
Dmitriy Shilin <dshil@fastmail.com>
2024-11-11 09:45:21 +0100

    sht41: read sensor's serial number on initialization

--- components/ocs_sensor/sht41/sensor.cpp
+++ components/ocs_sensor/sht41/sensor.cpp
@@ -43,6 +43,11 @@ Sensor::Sensor(io::i2c::ITransceiver& transceiver, Sensor::Params params)
     if (const auto code = reset_(); code != status::StatusCode::OK) {
         ocs_loge(log_tag, "failed to reset sensor: code=%s", status::code_to_str(code));
     }
+
+    if (const auto code = read_serial_number_(); code != status::StatusCode::OK) {
+        ocs_loge(log_tag, "failed to read sensor serial number: code=%s",
+                 status::code_to_str(code));
+    }
 }
 
 status::StatusCode Sensor::run() {
@@ -85,6 +90,46 @@ status::StatusCode Sensor::reset_() {
 
     return status::StatusCode::OK;
 }
+
+status::StatusCode Sensor::read_serial_number_() {
+    OCS_STATUS_RETURN_ON_ERROR(send_command_(Command::ReadSerialNumber));
+
+    uint8_t buf[6];
+    memset(buf, 0, sizeof(buf));
+    OCS_STATUS_RETURN_ON_ERROR(
+        transceiver_.receive(buf, sizeof(buf), params_.bus_wait_interval));
+
+    const uint16_t hi = algo::BitOps::pack_u8(buf[0], buf[1]);
+    const uint8_t hi_checksum = buf[2];
+    const uint8_t hi_checksum_calculated = calculate_crc(buf[0], buf[1]);
+    if (hi_checksum != hi_checksum_calculated) {
+        ocs_logw(
+            log_tag,
+            "failed to read serial number: invalid CRC for `hi` word: want=%u got=%u",
+            hi_checksum, hi_checksum_calculated);
+
+        return status::StatusCode::InvalidState;
+    }
+
+    const uint16_t lo = algo::BitOps::pack_u8(buf[3], buf[4]);
+    const uint8_t lo_checksum = buf[5];
+    const uint8_t lo_checksum_calculated = calculate_crc(buf[3], buf[4]);
+    if (lo_checksum != lo_checksum_calculated) {
+        ocs_logw(
+            log_tag,
+            "failed to read serial number: invalid CRC for `lo` word: want=%u got=%u",
+            lo_checksum, lo_checksum_calculated);
+
+        return status::StatusCode::InvalidState;
+    }
+
+    const uint32_t serial_number = algo::BitOps::pack_u16(hi, lo);
+
+    ocs_logi(log_tag, "serial_number=0x%8lX", serial_number);
+
+    return status::StatusCode::OK;
+}
+
 status::StatusCode Sensor::send_command_(Sensor::Command command) {
     OCS_STATUS_RETURN_ON_ERROR(
         transceiver_.send(reinterpret_cast<const uint8_t*>(&command), sizeof(command),
