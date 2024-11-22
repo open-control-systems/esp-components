a35b10a2c4e5851385ab1e080ebd0c0e41ad3c07
Dmitriy Shilin <dshil@fastmail.com>
2024-11-22 07:52:38 +0100

    Add FW description to registration data

--- components/ocs_pipeline/jsonfmt/registration_formatter.cpp
+++ components/ocs_pipeline/jsonfmt/registration_formatter.cpp
@@ -38,6 +38,7 @@ RegistrationFormatter::RegistrationFormatter(const system::DeviceInfo& device_in
     }
 
     version_formatter_->add("fw_name", device_info.get_fw_name());
+    version_formatter_->add("fw_description", device_info.get_fw_description());
     version_formatter_->add("device_id", device_info.get_device_id());
 
     fanout_formatter_->add(*version_formatter_);
