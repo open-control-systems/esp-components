88e9518b930f227cdd693211c96d0ab0564f7451
Dmitriy Shilin <dshil@fastmail.com>
2024-11-28 14:38:22 +0100

    Introduce basic support for Web GUI

---
+++ components/ocs_pipeline/httpserver/web_gui_pipeline.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024, Open Control Systems authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <vector>
+
+#include "ocs_core/noncopyable.h"
+#include "ocs_core/stream_transceiver.h"
+#include "ocs_http/server.h"
+#include "ocs_net/imdns_driver.h"
+
+namespace ocs {
+namespace pipeline {
+namespace httpserver {
+
+class WebGuiPipeline : public core::NonCopyable<> {
+public:
+    //! Initialize.
+    //!
+    //! @params
+    //!  - @p server to register endpoints to serve the Web GUI files.
+    //!  - @p mdns_driver to register mDNS txt records.
+    WebGuiPipeline(http::Server& server, net::IMdnsDriver& mdns_driver);
+
+private:
+    void initialize_fs_();
+    status::StatusCode handle_file_(httpd_req_t* req);
+
+    static const constexpr char* mount_point_ = "/web_gui";
+    static const size_t buffer_size_ = 1024;
+
+    bool valid_ { false };
+    core::StreamTransceiver::Buffer buffer_;
+};
+
+} // namespace httpserver
+} // namespace pipeline
+} // namespace ocs
